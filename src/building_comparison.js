41418106a126906f81527c1dbdb3eafe8d71af0e
Tristan Hildahl <thildahl20@my.whitworth.edu>
2019-03-03 17:45:53 -0800

    Edit Grid

--- src/building_comparison.js
+++ src/building_comparison.js
@@ -11,27 +11,27 @@ import BuildingList from "./components/BuildingList/buildingList.jsx"; // eslint
 
 const Index = () => { // eslint-disable-line no-unused-vars
     return [
-        <div class="container" id='app'>
-            <div class="row">
-                <div class="col-lg">
-                <Logo />
+        <div class='container' id='app'>
+            <div class='row'>
+                <div class='col-lg'>
+                    <Logo />
                 </div>
             </div>
-                {/* <div>Hello World!</div> */}
-                {/* <div className='alert alert-primary'>This is a Bootstrap Alert!</div> */}
-                {/* <div className='alert alert-danger'>This alert is Dangerous!</div> */}
-                {/* <Hello name='Team'></Hello> */}
-            <div class="row">
-                <div class="col-lg">
+            {/* <div>Hello World!</div> */}
+            {/* <div className='alert alert-primary'>This is a Bootstrap Alert!</div> */}
+            {/* <div className='alert alert-danger'>This alert is Dangerous!</div> */}
+            {/* <Hello name='Team'></Hello> */}
+            <div class='row'>
+                <div class='col-lg'>
                     <Nav />
                 </div>            
             </div>
 
-            <div class="row">
-                <div class="col-sm-3">
+            <div class='row'>
+                <div class='col-sm-3'>
                     <BuildingList />
                 </div>
-                <div class="col-sm-9">
+                <div class='col-sm-9'>
                 Graph here
                 </div>
             </div>
