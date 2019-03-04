29a2584511455fe8ad7ba071d2048566ffa356f0
Tristan Hildahl <thildahl20@my.whitworth.edu>
2019-03-03 17:06:24 -0800

    Test Checkbox Labels

--- src/components/BuildingList/buildingList.jsx
+++ src/components/BuildingList/buildingList.jsx
@@ -7,8 +7,20 @@ import { BuildingTab } from './styles.js'
 class BuildingList extends React.Component {
     render() {
           return (
-              <div class="col-3">
-                    <div class="list-group" id="myList" role="tablist">
+/*                 Attempt at checkboxes. Wrapping input in label puts them in line, but doesn't
+                   make them blocks, and blocks removes checkbox ability... Need to learn more on containers
+                   This must be it's own class, so don't leave below section and this "on"
+                   <div class="col-3">
+                    <div class="list-group">
+                    <a class="list-group-item list-group-action" data-toggle="list" href="#EJ" role="tab"><label><input type="checkbox"/>Eric Johnson</label></a> 
+                    <a class="list-group-item list-group-item-action" data-toggle="list" href="#Robinson" role="tab"><label><input type="checkbox"/>Robinson</label></a>
+                    </div>
+                    <label><input type="checkbox"/>Box3</label>
+
+                </div>
+ */
+                <div class="col-3">
+                    <div class="list-group" id="Buildings" role="tablist">
                     <a class="list-group-item list-group-item-action active" data-toggle="list" href="#EJ" role="tab">EJ</a> 
                     <a class="list-group-item list-group-item-action" data-toggle="list" href="#Robinson" role="tab">Robinson</a>
                     <a class="list-group-item list-group-item-action" data-toggle="list" href="#HUB" role="tab">HUB</a>
