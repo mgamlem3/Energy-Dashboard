c6df6ba5e254cceab024a5e0a2b121edbdf562ab
codefactor-io <support@codefactor.io>
2019-03-04 20:13:27 +0000

    [CodeFactor] Apply fixes

--- src/components/BuildingList/buildingList.jsx
+++ src/components/BuildingList/buildingList.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'bootstrap';
 
-import { BuildingTab } from './styles.js'
+import { BuildingTab } from './styles.js';
 
 class BuildingList extends React.Component {
     render() {
           return (
+
 /*                 Attempt at checkboxes. Wrapping input in label puts them in line, but doesn't
                    make them blocks, and blocks removes checkbox ability... Need to learn more on containers
                    This must be it's own class, so don't leave below section and this "on"
@@ -19,19 +20,20 @@ class BuildingList extends React.Component {
 
                 </div>
  */
-                <div id="buildingList">
-                    <div class="list-group" id="Buildings" role="tablist">
-                    <a class="list-group-item list-group-item-action active" data-toggle="list" href="#EJ" role="tab">EJ</a> 
-                    <a class="list-group-item list-group-item-action" data-toggle="list" href="#Robinson" role="tab">Robinson</a>
-                    <a class="list-group-item list-group-item-action" data-toggle="list" href="#HUB" role="tab">HUB</a>
-                    <a class="list-group-item list-group-item-action" data-toggle="list" href="#Library" role="tab">Library</a>
+
+                <div id='buildingList'>
+                    <div class='list-group' id='Buildings' role='tablist'>
+                    <a class='list-group-item list-group-item-action active' data-toggle='list' href='#EJ' role='tab'>EJ</a> 
+                    <a class='list-group-item list-group-item-action' data-toggle='list' href='#Robinson' role='tab'>Robinson</a>
+                    <a class='list-group-item list-group-item-action' data-toggle='list' href='#HUB' role='tab'>HUB</a>
+                    <a class='list-group-item list-group-item-action' data-toggle='list' href='#Library' role='tab'>Library</a>
                     </div>
 
-                    <div class="tab-content">
-                    <div class="tab-pane active" id="EJ" role="tabpanel">EJ</div> 
-                    <div class="tab-pane" id="Robinson" role="tabpanel">Rob</div>
-                    <div class="tab-pane" id="HUB" role="tabpanel">HUB</div>
-                    <div class="tab-pane" id="Library" role="tabpanel">Library</div>
+                    <div class='tab-content'>
+                    <div class='tab-pane active' id='EJ' role='tabpanel'>EJ</div> 
+                    <div class='tab-pane' id='Robinson' role='tabpanel'>Rob</div>
+                    <div class='tab-pane' id='HUB' role='tabpanel'>HUB</div>
+                    <div class='tab-pane' id='Library' role='tabpanel'>Library</div>
                     </div>
                 </div>
             );
--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Graphic } from './styles.js'
+import { Graphic } from './styles.js';
 
 class Logo extends React.Component {
     render() {
         return (
-            <div className = "Logo">
-                <Graphic src = "../src/resources/whitworth-logo-horizontal-rgb.png"/>
+            <div className = 'Logo'>
+                <Graphic src = '../src/resources/whitworth-logo-horizontal-rgb.png'/>
             </div>
-        )
+        );
     }
 }
 
