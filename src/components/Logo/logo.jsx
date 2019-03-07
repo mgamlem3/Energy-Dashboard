f3ba711582ef42390b3b465114e798a4caf7fe0d
Michael Gamlem III <mgamlem3@gmail.com>
2019-03-07 15:18:25 -0800

    Fix #35

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -4,9 +4,10 @@ import { Graphic } from './styles.js';
 
 class Logo extends React.Component {
     render() {
+        var image = require('../../resources/whitworth-logo-horizontal-rgb.png');
         return (
             <div className = 'Logo'>
-                <Graphic src = '../src/resources/whitworth-logo-horizontal-rgb.png' alt = 'Whitworth Logo'/>
+                <Graphic src = {image} alt = 'Whitworth Logo'/>
             </div>
         );
     }
