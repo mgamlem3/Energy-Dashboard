77dcb50eb1fd4d7ae1b508251a69a2f3f1502ea0
codefactor-io <support@codefactor.io>
2019-03-10 22:03:26 +0000

    [CodeFactor] Apply fixes to commit 91bdeb5

--- src/components/Graphs/LineGraph/line_graph.jsx
+++ src/components/Graphs/LineGraph/line_graph.jsx
@@ -2,58 +2,53 @@ import React from "react";
 var Chart = require("chart.js");
 
 class LineGraph extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    const context = this.context;
-
-    var myChart = new Chart(context, {
-      type: "line",
-      data: {
-        labels: ["Mar 7", "Mar 8", "Mar 9"],
-        datasets: [
-          {
-            label: "Energy Usage (Kw/hr)",
-            data: [1200, 1900, 1300],
-            backgroundColor: [
-              "rgba(194, 32, 51, 0.2)"
-
-/*               "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)" */
-
-            ]
-          }
-        ]
-      },
-      options: {
-        title: {
-            display: true,
-            text: 'Daily Energy Usage'
-        },
-        scales: {
-            yAxes: [{
-              ticks: {
-                  suggestedMin: 1000,
-                  suggestedMax: 2000
-              }
-            }]
-        }
+    constructor(props) {
+        super(props);
     }
-    });
-  }
+    componentDidMount() {
+        const context = this.context;
 
-  render() {
-    return (
-      <div>
-        <canvas
-          style={{ width: 500, height: 300 }}
-          ref={context => (this.context = context)}
-        />
-      </div>
-    );
-  }
+        var myChart = new Chart(context, {
+          type: "line",
+            data: {
+              labels: ["Mar 7", "Mar 8", "Mar 9"],
+              datasets: [
+                {
+                  label: "Energy Usage (Kw/hr)",
+                  data: [1200, 1900, 1300],
+                  backgroundColor: [
+                    "rgba(194, 32, 51, 0.2)"
+                  ]
+                }
+              ]
+            },
+          options: {
+            title: {
+                display: true,
+                text: 'Daily Energy Usage'
+            },
+            scales: {
+                yAxes: [{
+                  ticks: {
+                      suggestedMin: 1000,
+                      suggestedMax: 2000
+                  }
+                }]
+            }
+          }
+        });
+      }
+
+    render() {
+      return (
+        <div>
+          <canvas
+            style={{ width: 500, height: 300 }}
+            ref={context => (this.context = context)}
+          />
+        </div>
+      );
+    }
 }
 
 export default LineGraph;
