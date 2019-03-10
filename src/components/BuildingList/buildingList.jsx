ab1a31e057073b0c80228145b9f980a11158f7ec
Tristan Hildahl <thildahl20@my.whitworth.edu>
2019-03-10 12:28:57 -0700

    Add scroll bar to BuildingList

--- src/components/BuildingList/buildingList.jsx
+++ src/components/BuildingList/buildingList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "bootstrap";
 
+import { Buildings } from "./styles.js";
+
 class BuildingList extends React.Component {
     render() {
         return (
             <div id='buildingList'>
-                <div class='list-group' id='Buildings' role='tablist'>
+                <Buildings class='list-group' id='Buildings' role='tablist'>
                     <a class='list-group-item list-group-item-action' data-toggle='list' href='#Aquatic-Center' role='tab'>Aquatic Center</a>
                     <a class='list-group-item list-group-item-action' data-toggle='list' href='#Arend-Hall' role='tab'>Arend Hall</a>
                     <a class='list-group-item list-group-item-action' data-toggle='list' href='#Auld-House' role='tab'>Auld House</a>
@@ -21,7 +23,7 @@ class BuildingList extends React.Component {
                     <a class='list-group-item list-group-item-action' data-toggle='list' href='#Eric-Johnston' role='tab'>Eric Johnston</a> 
                     <a class='list-group-item list-group-item-action active' data-toggle='list' href='#HUB' role='tab'>HUB</a>
                     <a class='list-group-item list-group-item-action' data-toggle='list' href='#Robinson' role='tab'>Robinson</a>
-                </div>
+                </Buildings>
             </div>
         );
     }
