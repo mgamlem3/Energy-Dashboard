f6d0dbbec5dc8a1fa7f3f2782383d8b1530792f1
Michael Gamlem III <mgamlem3@gmail.com>
2019-03-12 13:51:16 -0700

    Fix Style Errors

--- src/components/Graphs/LineGraph/line_graph.jsx
+++ src/components/Graphs/LineGraph/line_graph.jsx
@@ -9,33 +9,33 @@ class LineGraph extends React.Component {
         const context = this.context;
 
         var myChart = new Chart(context, {
-          type: "line",
+            type: "line",
             data: {
-              labels: ["Mar 7", "Mar 8", "Mar 9"],
+                labels: ["Mar 7", "Mar 8", "Mar 9"],
               datasets: [
                 {
                   label: "Energy Usage (Kw/hr)",
                   data: [1200, 1900, 1300], // eslint-disable-line no-magic-numbers
-                  backgroundColor: [
-                    "rgba(194, 32, 51, 0.2)"
-                  ]
+                    backgroundColor: [
+                        "rgba(194, 32, 51, 0.2)"
+                    ]
                 }
               ]
             },
-          options: {
-            title: {
-                display: true,
-                text: 'Daily Energy Usage'
-            },
-            scales: {
-                yAxes: [{
-                  ticks: {
-                      suggestedMin: 1000,
-                      suggestedMax: 2000
-                  }
-                }]
+            options: {
+              title: {
+                  display: true,
+                  text: 'Daily Energy Usage'
+              },
+              scales: {
+                  yAxes: [{
+                    ticks: {
+                        suggestedMin: 1000,
+                        suggestedMax: 2000
+                    }
+                  }]
+              }
             }
-          }
         });
       }
 
