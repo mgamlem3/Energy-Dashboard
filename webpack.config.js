b9a04e556cc3a947d13a774d3f2a87fef5a107dd
Michael Gamlem III <mgamlem3@gmail.com>
2019-03-26 15:04:26 -0700

    add react hot dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require("path");
-
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
         index: "./src/index.js",
         buildingDetails: "./src/building_details.js",
         buildingComparison: "./src/building_comparison.js"
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        host: 'localhost',
+        port: 5000
     },
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -30,17 +36,18 @@ module.exports = {
         ]
     },
     plugins: [
-      new HtmlWebPackPlugin({
+        new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
-          }),
-      new HtmlWebPackPlugin({
+        }),
+        new HtmlWebPackPlugin({
             template: "./src/building_details.html",
             filename: "./building_details.html"
-          }),
-      new HtmlWebPackPlugin({
+        }),
+        new HtmlWebPackPlugin({
             template: "./src/building_comparison.html",
             filename: "./building_comparison.html"
-          })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
