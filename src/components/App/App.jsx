d939e995b216db10942dc0755e6f8203c2845ce7
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-02 17:05:09 -0700

    temp database test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+import 'bootstrap';
 import Header from "../Header/header.jsx";
 import HomePageContent from "../HomepageContent/homepage-content.jsx";
+import axios from 'axios';
 
 class App extends React.Component {
+    state = {
+        data: [],
+        id: 0,
+        message: "test 123"
+    }
+
+    getDataFromDb = () => {
+        fetch("http://localhost:5001/api/getData")
+          .then(data => data.json())
+          .then(res => console.log(res.data ));
+    };
+
+    putDataToDB = message => {
+        let currentIds = this.state.data.map(data => data.id);
+        let idToBeAdded = 0;
+        while (currentIds.includes(idToBeAdded)) {
+          ++idToBeAdded;
+        }
+
+        axios.post("http://localhost:5001/api/putData", {
+            id: idToBeAdded,
+            message: this.state.message
+        });
+    };
+    
     render() {
         return [
             <div id='app'>
                 <Header />
                 <HomePageContent />
+                <button onClick={this.getDataFromDb}>get data</button>
+                <button onClick={this.putDataToDB}>put</button>
             </div>
         ];
     }
