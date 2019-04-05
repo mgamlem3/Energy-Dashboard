f347313d7c540dc1beab38ab0c96174a1877ac94
codefactor-io <support@codefactor.io>
2019-04-05 08:09:40 +0000

    [CodeFactor] Apply fixes to commit 564d045

--- src/components/DetailspageContent/detailspage-content.jsx
+++ src/components/DetailspageContent/detailspage-content.jsx
@@ -25,6 +25,7 @@ class DetailsPageContent extends React.Component {
     
     updateData(){
         this.updatePie();
+
         //Need to get data from the database here using qualifiers
         var data = [1200, 1600, 1300, 1600, 1900, 1200];
         var labels = ["1", "2", "3", "4", "5", "6"];
