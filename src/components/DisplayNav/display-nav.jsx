cedaab1f63d0c414be9f7c0ca5209163eb11c8d0
Andrew Hutson <andrew.hutson@outlook.com>
2019-04-05 09:29:47 -0700

    Remove unused code from Display Nav

--- src/components/DisplayNav/display-nav.jsx
+++ src/components/DisplayNav/display-nav.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import "bootstrap";
 
 import Logo from "../Logo/logo.jsx";
-import { myNav } from "./styles.js";
 
 class DisplayNav extends React.Component {
     render() {
         return (
-            <myNav className='navbar'>
-            {/* <myNav className='navbar navbar-dark bg-dark'> */}
+            <div className='navbar'>
                 <a className='navbar-brand' href='./index.html'><Logo /></a>
-            </myNav>
+            </div>
         );
     }
 }
--- src/components/DisplayNav/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const SubMenu = styled.a`
-    color: LightGray;
-`;
-
-export const myNav = styled.nav`
-    height: 6vh;
-`;
