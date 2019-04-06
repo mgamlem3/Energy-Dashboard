e83f6609a01b7a3b8d0f759227dafc49589c7621
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-06 15:20:53 -0700

    # This is a combination of 3 commits. # This is the 1st commit message:
    modify schema to match our data
    
    # The commit message #1 will be skipped:
    
    # fixup schema
    
    # The commit message #3 will be skipped:
    
    # fixup

---
+++ src/database functions/api_functions.mjs
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export function getDataFromDatabase() {
+    fetch("http://localhost:5001/api/getData")
+        .then(data => data.json())
+        .then(res => console.log(res.data));
+};
+
+export function putDataToDatabase(message) {
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+        ++idToBeAdded;
+    }
+
+    axios.post("http://localhost:5001/api/putData", {
+        id: idToBeAdded,
+        message: this.state.message
+    });
+};
