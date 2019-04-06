b23765e8fbaaa30775f36fac8fdde2467a36c26d
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-06 15:21:25 -0700

    add database functions to be used throughout application
    # Conflicts:
    #	src/database functions/api_functions.mjs
    #	src/server/data.js

--- src/server/data.js
+++ src/server/data.js
@@ -5,11 +5,20 @@ const Schema = mongoose.Schema;
 // this will be our data base's data structure 
 const DataSchema = new Schema(
     {
+<<<<<<< HEAD
         date: String,
         building: String,
         peakDemand: String,
         peakTime: String,
         weeklyConsumption: String,
+=======
+        id: Number,
+        date: String,
+        building: String,
+        peakDemand: Number,
+        peakTime: String,
+        monthlyConsumption: Number,
+>>>>>>> 520a534... add database functions to be used throughout application
     },
     { timestamps: true }
 );
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 // modified from: https://medium.com/javascript-in-plain-english/full-stack-mongodb-react-node-js-express-js-in-one-simple-app-6cc8ed6de274
 
+/* eslint-disable no-console */
+
 const mongoose = require("mongoose");
 const express = require("express");
 var cors = require("cors");
@@ -15,14 +17,14 @@ const dbRoute = "mongodb://localhost:27017/newdb";
 
 // connects our back end code with the database
 mongoose.connect(
-  dbRoute,
-  { useNewUrlParser: true }
+    dbRoute,
+    { useNewUrlParser: true }
 );
 
 let db = mongoose.connection;
 
 db.once("open", () => console.log("connected to the database"));
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("error", console.error.bind(console, "MongoDB connection error:")); 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -30,55 +32,56 @@ app.use(logger("dev"));
 
 // this method fetches all available data in our database
 router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
+    Data.find((err, data) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: data });
+    });
 });
 
 // this method overwrites existing data in our database
 router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findOneAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
+    const { id, update } = req.body;
+    Data.findOneAndUpdate(id, update, err => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
 });
 
 // this method removes existing data in our database
 router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findOneAndDelete(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
+    const { id } = req.body;
+    Data.findOneAndDelete(id, err => {
+        if (err) return res.send(err);
+        return res.json({ success: true });
+    });
 });
 
 // this method adds new data in our database
 router.post("/putData", (req, res) => {
-  let data = new Data();
+    let data = new Data();
 
-  const { id, date, building, peakDemand, peakTime, monthlyConsumption } = req.body;
+    const { id, date, building, peakDemand, peakTime, monthlyConsumption } = req.body;
 
-  if ((!id && id !== 0) || !building) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
+    if ((!id && id !== 0) || !building) { // eslint-disable-line no-magic-numbers
+        return res.json({
+            success: false,
+            error: "INVALID INPUTS"
+        });
+    }
+
+    // fill fields
+    data.id = id;
+    data.date = date;
+    data.building = building;
+    data.peakDemand = peakDemand;
+    data.peakTime = peakTime;
+    data.monthlyConsumption = monthlyConsumption;
+
+    // save object
+    data.save(err => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
     });
-  }
-  // fill fields
-  data.id = id;
-  data.date = date;
-  data.building = building;
-  data.peakDemand = peakDemand;
-  data.peakTime = peakTime;
-  data.monthlyConsumption = monthlyConsumption;
-
-  // save object
-  data.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
 });
 
 // append /api for our http requests
