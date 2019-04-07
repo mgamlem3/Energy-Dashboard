a6fe8c84a99075eca85894bf95be4197ea7df52a
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-07 14:27:25 -0700

    Add JS docs to api_functions

--- src/database functions/api_functions.mjs
+++ src/database functions/api_functions.mjs
@@ -1,3 +1,7 @@
+/**
+ * @author Michael Gamlem III
+ */
+
 import axios from 'axios';
 
 const API_URL = "http://localhost:5001/api";
@@ -11,6 +15,10 @@ export function getDataFromDatabase() {
         .then(res => console.log(res.data));
 };
 
+/**
+ * @param message optional message to append to database
+ * @description Use this function to add data to the database. The body of the post must have the data that is to be added to the database
+ */
 export function putDataToDatabase(message) {
     let currentIds = this.state.data.map(data => data.id);
     let idToBeAdded = 0;
@@ -24,6 +32,11 @@ export function putDataToDatabase(message) {
     });
 };
 
+/**
+ * @async
+ * @param building Building that is to be searched for in the database
+ * @description This function will return the most recent entry for one building
+ */
 export function getMostRecentEntryForBuilding(building) {
     var url = new URL(API_URL+'/mostRecent')
 
