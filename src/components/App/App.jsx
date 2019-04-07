1ac92f9e13a08a63fa192d796b1d3e32eab53426
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-07 16:24:50 -0700

    add temp test most recent entries button to homepage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "../Header/header.jsx";
 import HomePageContent from "../HomepageContent/homepage-content.jsx";
-import { getDataFromDatabase, getMostRecentEntryForBuilding } from "../../database functions/api_functions.mjs";
+import { getDataFromDatabase, getMostRecentEntryForBuilding, getMostRecentEntriesForBuilding, convertResponseToArrays } from "../../database functions/api_functions.mjs";
 
 class App extends React.Component {    
     async logBuildingData() {
         var response = await getMostRecentEntryForBuilding("All");
         console.log(response.data);
+    }
+
+    async mostRecentEntries() {
+        var response = await getMostRecentEntriesForBuilding("Boppel", 6);
+        console.log(response.data);
+        var data = convertResponseToArrays(response);
+        console.log(data);
     }
     
     render() {
@@ -16,6 +23,7 @@ class App extends React.Component {
                 <HomePageContent />
                 <button onClick = {getDataFromDatabase}>get all data from database</button>
                 <button onClick={this.logBuildingData}>get most recent data for all campus</button>
+                <button onClick={this.mostRecentEntries}>get six most recent entries for Boppel</button>
             </div>
         ];
     }
