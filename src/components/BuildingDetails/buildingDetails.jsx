23657c8594f68357a5a5b626544dca49820152ba
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-10 10:56:55 -0700

    Add Common Resources for building search names

--- src/components/BuildingDetails/buildingDetails.jsx
+++ src/components/BuildingDetails/buildingDetails.jsx
@@ -1,50 +1,51 @@
 import React from "react";
 import "bootstrap";
+import { buildings } from '../../resources/common-text-resources';
 
 class BuildingDetails extends React.Component {
     render() {
         return (
             <div className='tab-content'>
-                <div className='tab-pane' id='Aquatic-Center' role='tabpanel'>Aquatic Center</div>
-                <div className='tab-pane' id='Arend-Hall' role='tabpanel'>Arend Hall</div>
-                <div className='tab-pane' id='Auld-House' role='tabpanel'>Auld House</div>
-                <div className='tab-pane' id='Baldwin-Jenkins-Hall' role='tabpanel'>Baldwin-Jenkins Hall</div>
-                <div className='tab-pane' id='Ballard-Hall' role='tabpanel'>Ballard Hall</div>
-                <div className='tab-pane' id='Beeksma-Family-Theology-Center' role='tabpanel'>Beeksma Family Theology Center</div>
-                <div className='tab-pane' id='Boppell-Hall' role='tabpanel'>Boppell Hall</div>
-                <div className='tab-pane' id='Cowles-Auditorium' role='tabpanel'>Cowles Auditorium</div>
-                <div className='tab-pane' id='Cowles-Library' role='tabpanel'>Cowles Library</div>
-                <div className='tab-pane' id='Cowles-Music-Center' role='tabpanel'>Cowles Music Center</div>
-                <div className='tab-pane' id='Dixon-Hall' role='tabpanel'>Dixon Hall</div>
-                <div className='tab-pane' id='Duvall-Hall' role='tabpanel'>Duvall Hall</div>
-                <div className='tab-pane' id='Eric-Johnston' role='tabpanel'>Eric Johnston</div>
-                <div className='tab-pane' id='Facilities-Services' role='tabpanel'>Facilities Services</div>
-                <div className='tab-pane' id='Fieldhouse' role='tabpanel'>Fieldhouse</div>
-                <div className='tab-pane' id='Graham-House' role='tabpanel'>Graham House</div>
-                <div className='tab-pane' id='Graves-Gym' role='tabpanel'>Graves Gym</div>
-                <div className='tab-pane' id='Hardwick-House' role='tabpanel'>Hardwick House</div>
-                <div className='tab-pane' id='Hawthorne-Hall' role='tabpanel'>Hawthorne Hall</div>
-                <div className='tab-pane' id='Hendrick-Hall' role='tabpanel'>Hendrick Hall</div>
-                <div className='tab-pane' id='Hill-House' role='tabpanel'>Hill House</div>
-                <div className='tab-pane active' id='HUB' role='tabpanel'>HUB</div>
-                <div className='tab-pane' id='Lied-Center' role='tabpanel'>Lied Center for the Visual Arts</div>
-                <div className='tab-pane' id='Lindaman-Center' role='tabpanel'>Lindaman Center</div>
-                <div className='tab-pane' id='MacKay-Hall' role='tabpanel'>MacKay Hall</div>
-                <div className='tab-pane' id='McEachran-Hall' role='tabpanel'>McEachran Hall</div>
-                <div className='tab-pane' id='McMillan-Hall' role='tabpanel'>McMillan Hall</div>
-                <div className='tab-pane' id='Oliver-Hall' role='tabpanel'>Oliver Hall</div>
-                <div className='tab-pane' id='Robinson' role='tabpanel'>Robinson</div>
-                <div className='tab-pane' id='Schumacher-Hall' role='tabpanel'>Schumacher Hall</div>
-                <div className='tab-pane' id='Stewart-Hall' role='tabpanel'>Stewart Hall</div>
-                <div className='tab-pane' id='Tacoma-Hall' role='tabpanel'>Tacoma Hall</div>
-                <div className='tab-pane' id='Tennis-Bubble' role='tabpanel'>Tennis Bubble</div>
-                <div className='tab-pane' id='UREC' role='tabpanel'>University Recreation Center</div>
-                <div className='tab-pane' id='Village-Akili' role='tabpanel'>The Village (Akili)</div>
-                <div className='tab-pane' id='Village-Shalom' role='tabpanel'>The Village (Shalom)</div>
-                <div className='tab-pane' id='Village-Tiki' role='tabpanel'>The Village (Tiki)</div>
-                <div className='tab-pane' id='Warren-Hall' role='tabpanel'>Warren Hall</div>
-                <div className='tab-pane' id='Westminster-Hall' role='tabpanel'>Westminster Hall</div>
-                <div className='tab-pane' id='Weyerhaeuser-Hall' role='tabpanel'>Weyerhaeuser Hall</div>
+                <div className='tab-pane' id={buildings.AquaticCenter} role='tabpanel'>Aquatic Center</div>
+                <div className='tab-pane' id={buildings.ArendHall} role='tabpanel'>Arend Hall</div>
+                <div className='tab-pane' id={buildings.AuldHouse} role='tabpanel'>Auld House</div>
+                <div className='tab-pane' id={buildings.BaldwinJenkinsHall} role='tabpanel'>Baldwin-Jenkins Hall</div>
+                <div className='tab-pane' id={buildings.BallardHall} role='tabpanel'>Ballard Hall</div>
+                <div className='tab-pane' id={buildings.BeeksmaFamilyTheologyCenter} role='tabpanel'>Beeksma Family Theology Center</div>
+                <div className='tab-pane' id={buildings.BoppellHall} role='tabpanel'>Boppell Hall</div>
+                <div className='tab-pane' id={buildings.CowlesAuditorium} role='tabpanel'>Cowles Auditorium</div>
+                <div className='tab-pane' id={buildings.CowlesLibrary} role='tabpanel'>Cowles Library</div>
+                <div className='tab-pane' id={buildings.CowlesMusicCenter} role='tabpanel'>Cowles Music Center</div>
+                <div className='tab-pane' id={buildings.DixonHall} role='tabpanel'>Dixon Hall</div>
+                <div className='tab-pane' id={buildings.DuvallHall} role='tabpanel'>Duvall Hall</div>
+                <div className='tab-pane' id={buildings.EricJohnston} role='tabpanel'>Eric Johnston</div>
+                <div className='tab-pane' id={buildings.FacilitiesServices} role='tabpanel'>Facilities Services</div>
+                <div className='tab-pane' id={buildings.Fieldhouse} role='tabpanel'>Fieldhouse</div>
+                <div className='tab-pane' id={buildings.GrahmHouse} role='tabpanel'>Graham House</div>
+                <div className='tab-pane' id={buildings.GravesGym} role='tabpanel'>Graves Gym</div>
+                <div className='tab-pane' id={buildings.HardwickHouse} role='tabpanel'>Hardwick House</div>
+                <div className='tab-pane' id={buildings.HawthorneHall} role='tabpanel'>Hawthorne Hall</div>
+                <div className='tab-pane' id={buildings.HendrickHall} role='tabpanel'>Hendrick Hall</div>
+                <div className='tab-pane' id={buildings.HillHouse} role='tabpanel'>Hill House</div>
+                <div className='tab-pane active' id={buildings.HUB} role='tabpanel'>HUB</div>
+                <div className='tab-pane' id={buildings.LeidCenter} role='tabpanel'>Lied Center for the Visual Arts</div>
+                <div className='tab-pane' id={buildings.LindamanCenter} role='tabpanel'>Lindaman Center</div>
+                <div className='tab-pane' id={buildings.MacKayHall} role='tabpanel'>MacKay Hall</div>
+                <div className='tab-pane' id={buildings.McEachranHall} role='tabpanel'>McEachran Hall</div>
+                <div className='tab-pane' id={buildings.McMillanHall} role='tabpanel'>McMillan Hall</div>
+                <div className='tab-pane' id={buildings.OliverHall} role='tabpanel'>Oliver Hall</div>
+                <div className='tab-pane' id={buildings.Robinson} role='tabpanel'>Robinson</div>
+                <div className='tab-pane' id={buildings.SchumacherHall} role='tabpanel'>Schumacher Hall</div>
+                <div className='tab-pane' id={buildings.StewartHall} role='tabpanel'>Stewart Hall</div>
+                <div className='tab-pane' id={buildings.TacomaHall} role='tabpanel'>Tacoma Hall</div>
+                <div className='tab-pane' id={buildings.TennisBubble} role='tabpanel'>Tennis Bubble</div>
+                <div className='tab-pane' id={buildings.UREC} role='tabpanel'>University Recreation Center</div>
+                <div className='tab-pane' id={buildings.VillageAkili} role='tabpanel'>The Village (Akili)</div>
+                <div className='tab-pane' id={buildings.VillageShalom} role='tabpanel'>The Village (Shalom)</div>
+                <div className='tab-pane' id={buildings.VillageTiki} role='tabpanel'>The Village (Tiki)</div>
+                <div className='tab-pane' id={buildings.WarrenHall} role='tabpanel'>Warren Hall</div>
+                <div className='tab-pane' id={buildings.WestminsterHall} role='tabpanel'>Westminster Hall</div>
+                <div className='tab-pane' id={buildings.WeyerhaeuserHall} role='tabpanel'>Weyerhaeuser Hall</div>
             </div>
         );
     }
--- src/components/BuildingList/buildingList.jsx
+++ src/components/BuildingList/buildingList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap";
-
-import { Buildings } from "./styles.js";
+import { buildings } from '../../resources/common-text-resources';
+import { BuildingsSidepane } from "./styles.js";
 
 class BuildingList extends React.Component {
     constructor(props) {
@@ -17,37 +17,37 @@ class BuildingList extends React.Component {
         return (
             <div className='container-fluid'>
                 <div id='buildingList'>
-                    <Buildings className='list-group' id='Buildings' role='tablist'>
-                        <button className='list-group-item list-group-item-action' id='Aquatic-Center' onClick={this.clickBuilding} data-toggle='list' href='#Aquatic-Center' role='tab'>Aquatic Center</button>
-                        <button className='list-group-item list-group-item-action' id='Arend-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Arend-Hall' role='tab'>Arend Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Auld-House' onClick={this.clickBuilding} data-toggle='list' href='#Auld-House' role='tab'>Auld House</button>
-                        <button className='list-group-item list-group-item-action' id='Baldwin-Jenkins-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Baldwin-Jenkins-Hall' role='tab'>Baldin-Jenkins Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Ballard-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Ballard-Hall' role='tab'>Ballard Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Beeksma-Family-Theology-Center' onClick={this.clickBuilding} data-toggle='list' href='#Beeksma-Family-Theology-Center' role='tab'>Beeksma Family Theology Center</button>
-                        <button className='list-group-item list-group-item-action' id='Boppell-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Boppell-Hall' role='tab'>Boppell Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Cowles-Auditorium' onClick={this.clickBuilding} data-toggle='list' href='#Cowles-Auditorium' role='tab'>Cowles Auditorium</button>
-                        <button className='list-group-item list-group-item-action' id='Cowles-Library' onClick={this.clickBuilding} data-toggle='list' href='#Cowles-Library' role='tab'>Cowles Library</button>
-                        <button className='list-group-item list-group-item-action' id='Cowles-Music-Center' onClick={this.clickBuilding} data-toggle='list' href='#Cowles-Music-Center' role='tab'>Cowles Music Center</button>
-                        <button className='list-group-item list-group-item-action' id='Dixon-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Dixon-Hall' role='tab'>Dixon Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Duvall-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Duvall-Hall' role='tab'>Duvall Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Eric-Johnston' onClick={this.clickBuilding} data-toggle='list' href='#Eric-Johnston' role='tab'>Eric Johnston</button>
-                        <button className='list-group-item list-group-item-action' id='Facilities-Services' onClick={this.clickBuilding} data-toggle='list' href='#Facilities-Services' role='tab'>Facilities Services</button>
-                        <button className='list-group-item list-group-item-action' id='Fieldhouse' onClick={this.clickBuilding} data-toggle='list' href='#Fieldhouse' role='tab'>Fieldhouse</button>
-                        <button className='list-group-item list-group-item-action' id='Graham-House' onClick={this.clickBuilding} data-toggle='list' href='#Graham-House' role='tab'>Graham House</button>
-                        <button className='list-group-item list-group-item-action' id='Graves-Gym' onClick={this.clickBuilding} data-toggle='list' href='#Graves-Gym' role='tab'>Graves Gym</button>
-                        <button className='list-group-item list-group-item-action' id='Hardwick-House' onClick={this.clickBuilding} data-toggle='list' href='#Hardwick-House' role='tab'>Hardwick House</button>
-                        <button className='list-group-item list-group-item-action' id='Hawthorne-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Hawthorne-Hall' role='tab'>Hawthorne Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Hendrick-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Hendrick-Hall' role='tab'>Hendrick Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Hill-House' onClick={this.clickBuilding} data-toggle='list' href='#Hill-House' role='tab'>Hill House</button>
-                        <button className='list-group-item list-group-item-action active' id='HUB' onClick={this.clickBuilding} data-toggle='list' href='#HUB' role='tab'>HUB</button>
-                        <button className='list-group-item list-group-item-action' id='Lied-Center' onClick={this.clickBuilding} data-toggle='list' href='#Lied-Center' role='tab'>Lied Center for the Visual Arts</button>
-                        <button className='list-group-item list-group-item-action' id='Lindaman-Center' onClick={this.clickBuilding} data-toggle='list' href='#Lindaman-Center' role='tab'>Lindaman Center</button>
-                        <button className='list-group-item list-group-item-action' id='MacKay-Hall' onClick={this.clickBuilding} data-toggle='list' href='#MacKay-Hall' role='tab'>MacKay Hall</button>
-                        <button className='list-group-item list-group-item-action' id='McEachran-Hall' onClick={this.clickBuilding} data-toggle='list' href='#McEachran-Hall' role='tab'>McEachran Hall</button>
-                        <button className='list-group-item list-group-item-action' id='McMillan-Hall' onClick={this.clickBuilding} data-toggle='list' href='#McMillan-Hall' role='tab'>McMillan Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Oliver-Hall' onClick={this.clickBuilding} data-toggle='list' href='#Oliver-Hall' role='tab'>Oliver Hall</button>
-                        <button className='list-group-item list-group-item-action' id='Robinson' onClick={this.clickBuilding} data-toggle='list' href='#Robinson' role='tab'>Robinson</button>
-                    </Buildings>
+                    <BuildingsSidepane className='list-group' id='Buildings' role='tablist'>
+                        <button className='list-group-item list-group-item-action' id={buildings.AquaticCenter} onClick={this.clickBuilding} data-toggle='list' href='#Aquatic-Center' role='tab'>Aquatic Center</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.ArendHall} onClick={this.clickBuilding} data-toggle='list' href='#Arend-Hall' role='tab'>Arend Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.AuldHouse} onClick={this.clickBuilding} data-toggle='list' href='#Auld-House' role='tab'>Auld House</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.BaldwinJenkinsHall} onClick={this.clickBuilding} data-toggle='list' href='#Baldwin-Jenkins-Hall' role='tab'>Baldin-Jenkins Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.BallardHall} onClick={this.clickBuilding} data-toggle='list' href='#Ballard-Hall' role='tab'>Ballard Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.BeeksmaFamilyTheologyCenter} onClick={this.clickBuilding} data-toggle='list' href='#Beeksma-Family-Theology-Center' role='tab'>Beeksma Family Theology Center</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.BoppellHall} onClick={this.clickBuilding} data-toggle='list' href='#Boppell-Hall' role='tab'>Boppell Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.CowlesAuditorium} onClick={this.clickBuilding} data-toggle='list' href='#Cowles-Auditorium' role='tab'>Cowles Auditorium</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.CowlesLibrary} onClick={this.clickBuilding} data-toggle='list' href='#Cowles-Library' role='tab'>Cowles Library</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.CowlesMusicCenter} onClick={this.clickBuilding} data-toggle='list' href='#Cowles-Music-Center' role='tab'>Cowles Music Center</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.DixonHall} onClick={this.clickBuilding} data-toggle='list' href='#Dixon-Hall' role='tab'>Dixon Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.DuvallHall} onClick={this.clickBuilding} data-toggle='list' href='#Duvall-Hall' role='tab'>Duvall Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.EricJohnston} onClick={this.clickBuilding} data-toggle='list' href='#Eric-Johnston' role='tab'>Eric Johnston</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.FacilitiesServices} onClick={this.clickBuilding} data-toggle='list' href='#Facilities-Services' role='tab'>Facilities Services</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.Fieldhouse} onClick={this.clickBuilding} data-toggle='list' href='#Fieldhouse' role='tab'>Fieldhouse</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.GrahamHouse} onClick={this.clickBuilding} data-toggle='list' href='#Graham-House' role='tab'>Graham House</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.GravesGym} onClick={this.clickBuilding} data-toggle='list' href='#Graves-Gym' role='tab'>Graves Gym</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.HardwickHouse} onClick={this.clickBuilding} data-toggle='list' href='#Hardwick-House' role='tab'>Hardwick House</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.HawthorneHall} onClick={this.clickBuilding} data-toggle='list' href='#Hawthorne-Hall' role='tab'>Hawthorne Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.HendrickHall} onClick={this.clickBuilding} data-toggle='list' href='#Hendrick-Hall' role='tab'>Hendrick Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.HillHouse} onClick={this.clickBuilding} data-toggle='list' href='#Hill-House' role='tab'>Hill House</button>
+                        <button className='list-group-item list-group-item-action active' id={buildings.HUB} onClick={this.clickBuilding} data-toggle='list' href='#HUB' role='tab'>HUB</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.LiedCenter} onClick={this.clickBuilding} data-toggle='list' href='#Lied-Center' role='tab'>Lied Center for the Visual Arts</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.LindamanCenter} onClick={this.clickBuilding} data-toggle='list' href='#Lindaman-Center' role='tab'>Lindaman Center</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.MacKayHall} onClick={this.clickBuilding} data-toggle='list' href='#MacKay-Hall' role='tab'>MacKay Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.McEachranHall} onClick={this.clickBuilding} data-toggle='list' href='#McEachran-Hall' role='tab'>McEachran Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.McMillanHall} onClick={this.clickBuilding} data-toggle='list' href='#McMillan-Hall' role='tab'>McMillan Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.OliverHall} onClick={this.clickBuilding} data-toggle='list' href='#Oliver-Hall' role='tab'>Oliver Hall</button>
+                        <button className='list-group-item list-group-item-action' id={buildings.Robinson} onClick={this.clickBuilding} data-toggle='list' href='#Robinson' role='tab'>Robinson</button>
+                    </BuildingsSidepane>
                 </div>
             </div>
         );
