d18331587bb0e84ad839d57881b0a61b7974105a
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-19 19:41:08 -0700

    add function to average values

---
+++ src/server/process_data.js
@@ -0,0 +1,22 @@
+/**
+ * @author Michael Gamlem III
+ */
+
+/**
+ * @description This function will average all elements of an array
+ * @param values array of numbers
+ * @returns average of values or null if error
+ */
+export function average(values) {
+    var sum, average = 0;
+    try {
+        values.forEach(value => {
+            sum += value;
+        });
+        average = sum/values.length();
+    } catch (error) {
+        console.error("ERROR: Unable to average values");
+        average = null;
+    }
+    return average;
+}
