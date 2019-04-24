45068af99d0eb2791bbfca2875cc151f72c3d925
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-23 22:16:56 -0700

    Add function to separate data points into years

--- src/server/process_data.js
+++ src/server/process_data.js
@@ -13,11 +13,34 @@ export function average(values) {
     try {
         values.forEach(value => {
             sum += value;
+    /**
+     * @description This function will turn results from the database into three separate year arrays
+     * @param {array: Number} values 
+     * @param {object} NOW current date/time data from `server.js`
+     * @returns object with three arrays (thisYear, lastYear, lastLastYear)
+     */
+    separateDataIntoYears : function(values, NOW) {
+        var ret = {
+            thisYear: [],
+            lastYear: [],
+            lastLastYear: []
+        };
+
+        values.forEach(entry => {
+            if(entry.date >= NOW.today - Constants.THREE_YEARS_AGO && entry.date <= NOW.today - Constants.TWO_YEARS_AGO) {
+                // three years old
+                ret.lastLastYear.push(entry);
+            } else if (entry.date >= NOW.today - Constants.TWO_YEARS_AGO && entry.date <= NOW.today - Constants.ONE_YEAR_AGO) {
+                // two years old
+                ret.lastYear.push(entry);                
+            } else if (entry.date >= NOW.today - Constants.ONE_YEAR_AGO && entry.date <= NOW.today) {
+                // one year old
+                ret.thisYear.push(entry);
+            } else {
+                console.warn("Entry with date %s is not able to be parsed. Is it more than three years old?", entry.date);
+            }
         });
-        average = sum/values.length();
-    } catch (error) {
-        console.error("ERROR: Unable to average values");
-        average = null;
+
+        return ret;
     }
-    return average;
 }
