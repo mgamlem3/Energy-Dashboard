83175ab7856838c10d65818b0aa6ee117173cc62
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-23 22:24:21 -0700

    add object for main graph return

---
+++ src/server/MainGraphDataReturn.js
@@ -0,0 +1,14 @@
+module.exports = {
+    MainGraphDataReturn : function() {
+        thisYearData = [];
+        thisYearLabels = [];
+        lastYearData = [];
+        lastYearLabelsData = [];
+        lastLastYearData = [];
+        lastLastYearLabels = [];
+        last12MonthsData = [];
+        last12MonthsLabels = [];
+        last24HoursData = [];
+        last24HoursLabels = [];
+    }
+}
