6953c9d4b9b3f10260914407f1cdffc5cfc1b05e
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-24 13:12:07 -0700

    Complete getMonthlyAverages

--- src/server/process_data.js
+++ src/server/process_data.js
@@ -59,34 +59,42 @@ module.exports = {
      * @description This function will turn results from the database into three separate year arrays
      * @param {array: Number} values 
      * @param {object} NOW current date/time data from `server.js`
-     * @returns object with three arrays (thisYear, lastYear, lastLastYear)
+     * @returns object with three arrays of monthly averages for each year (thisYear, lastYear, lastLastYear)
      */
 
-    separateDataIntoYears : function(values, NOW) {
+    getMonthAverages : function(values, NOW) {
         var ret = {
             thisYear: [],
             lastYear: [],
             lastLastYear: []
         };
 
-        values.forEach(entry => {
-            if(entry.date >= NOW.today - Constants.THREE_YEARS_AGO && entry.date <= NOW.today - Constants.TWO_YEARS_AGO) {
+        // sort all datapoints into proper year
+        try {
+            values.forEach(entry => {
+                if(entry.date >= NOW.today - Constants.THREE_YEARS_AGO && entry.date <= NOW.today - Constants.TWO_YEARS_AGO) {
 
-                // three years old
-                ret.lastLastYear.push(entry);
-            } else if (entry.date >= NOW.today - Constants.TWO_YEARS_AGO && entry.date <= NOW.today - Constants.ONE_YEAR_AGO) {
+                    // three years old
+                    ret.lastLastYear.push(entry);
+                } else if (entry.date >= NOW.today - Constants.TWO_YEARS_AGO && entry.date <= NOW.today - Constants.ONE_YEAR_AGO) {
 
-                // two years old
-                ret.lastYear.push(entry);                
-            } else if (entry.date >= NOW.today - Constants.ONE_YEAR_AGO && entry.date <= NOW.today) {
+                    // two years old
+                    ret.lastYear.push(entry);                
+                } else if (entry.date >= NOW.today - Constants.ONE_YEAR_AGO && entry.date <= NOW.today) {
 
-                // one year old
-                ret.thisYear.push(entry);
-            } else {
-                console.warn("Entry with date %s is not able to be parsed. Is it more than three years old?", entry.date);
-            }
-        });
+                    // one year old
+                    ret.thisYear.push(entry);
+                } else {
+                    console.warn("Entry with date %s is not able to be parsed. Is it more than three years old?", entry.date);
+                }
+            });
+        } catch (e) {
+            console.error("Error while processing data and sorting into years." + e);
+        }
 
+        //get average for each month of the year
+        ret = getMonthlyAverages(ret);
+        
         return ret;
     }
 };
@@ -115,3 +123,45 @@ function average(values) {
     return average;
 }
 
+/**
+ * @description This function is meant to convert all entries from a month into averages that will be returned to the `getMonthAverages()` function. It is not intended to be called by anything else.
+ * @param {Object: 3 arrays} arrays must be in the shape of the `ret` object from `getMonthAverages()` above 
+ * @returns three arrays with monthly averaged values. (If no data can be found for the month, a zero is entered).
+ */
+function getMonthlyAverages(arrays) {
+    var currentMonth = array[0].date.getMonth();
+    var vals = [];
+    var ret = {
+        thisYear = [],
+        lastYear = [],
+        lastLastYear = [],
+    }
+    arrays.thisYear.forEach(element => {
+        if (element.date.getMonth() == currentMonth) {
+            vals.push(element.date.peakDemand)
+        } else {
+            currentMonth = element.date.getMonth();
+            ret.thisYear.push(average(vals));
+        }
+    });
+    arrays.lastYear.forEach(element => {
+        vals = [];
+        if (element.date.getMonth() == currentMonth) {
+            vals.push(element.date.peakDemand)
+        } else {
+            currentMonth = element.date.getMonth();
+            ret.lastYear.push(average(vals));
+        }
+    });
+    arrays.lastLastYear.forEach(element => {
+        vals = [];
+        if (element.date.getMonth() == currentMonth) {
+            vals.push(element.date.peakDemand)
+        } else {
+            currentMonth = element.date.getMonth();
+            ret.lastLastYear.push(average(vals));
+        }
+    });
+
+    return ret;
+}
