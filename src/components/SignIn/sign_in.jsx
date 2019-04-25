6c98e07d0a0aec32d3ff57a99e3b57baec0a8cdc
Andrew Hutson <andrew.hutson@outlook.com>
2019-04-25 15:21:58 -0700

    Add spacing to sign in page

--- src/components/SignIn/sign_in.jsx
+++ src/components/SignIn/sign_in.jsx
@@ -33,16 +33,20 @@ class SignIn extends React.Component {
                                 <form className='form-signin'>
                                     <label>Username</label>
                                     <input type='text' className='form-control' required autofocus></input>
+                                    <div className='row mt-2'></div>
                                     <label>Password</label>
                                     <input type='password' className='form-control' required></input>
+                                    <div className='row mt-2'></div>
                                     <label>
                                     <input type='checkbox' name='remember'></input>
                                         Remember me
                                     </label>
+                                    <div className='row mt-2'></div>
                                     <button className='btn btn-lg btn-outline-primary btn-block' href='./management.html' type='submit'>
                                         Sign In
                                     </button>
                                 </form>
+                                <div className='row mt-2'></div>
                                 <BottomButton className="btn btn-outline-secondary btn-block" onClick={this.toggleSignUpView}>Sign Up</BottomButton>
                             </div>
                         </div>
@@ -50,23 +54,24 @@ class SignIn extends React.Component {
                     {/* Sign Up View */}
                     {this.state.signUp &&
                         <div className="SignUpView">
-                            <h1 className='text-center login-title'>Sign Up to continue</h1>
+                            <h1 className='text-center login-title'>Sign up to continue</h1>
                             <div className='account-wall'>
                                 <form className='form-signup'>
                                     <label>Username</label>
                                     <input type='text' class='form-control' required autofocus></input>
+                                    <div className='row mt-2'></div>
                                     <label>Password</label>
                                     <input type='password' className='form-control' required></input>
+                                    <div className='row mt-2'></div>
                                     <label>Confirm Password</label>
                                     <input type='password' className='form-control' required></input>
-                                    <label>
-                                    <input type='checkbox' name='remember'></input>
-                                        Remember me
-                                    </label>
+                                    <div className='row mt-2'></div>
+                                    <div className='row mt-2'></div>
                                     <button className='btn btn-lg btn-outline-primary btn-block' href='./management.html' type='submit'>
                                         Sign Up
                                     </button>
                                 </form>
+                                <div className='row mt-2'></div>
                                 <BottomButton className="btn btn-outline-secondary btn-block" onClick={this.toggleSignUpView}>Go Back to Sign In</BottomButton>
                             </div>
                         </div>
