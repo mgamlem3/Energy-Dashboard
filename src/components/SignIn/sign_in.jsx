f77d0324291da1efa78cb1eac801a11a2e0a753a
Michael Gamlem III <mgamlem3@gmail.com>
2019-04-25 14:45:20 -0700

    Update Sign In component to also function as sign up component

--- src/components/SignIn/sign_in.jsx
+++ src/components/SignIn/sign_in.jsx
@@ -1,31 +1,77 @@
 import React from "react";
 
-import { Graphic } from "./styles.js";
+import { Graphic, BottomButton } from "./styles.js";
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+require("firebase/auth");
 
 class SignIn extends React.Component {
+    
+    state = {
+        signUp: false
+    }
+
+    toggleSignUpView = () => {
+        this.setState({signUp: !this.state.signUp});
+    }
+
     render() {
         var image = require("../../resources/whitworth-logo-horizontal-rgb.png");
         return (
             <div className='SignIn'>
-            <div className='row justify-content-center align-items-center'>
-                <Graphic src = {image} alt = 'Whitworth Logo'/>
-            </div>
-                <h1 className='text-center login-title'>Sign in to continue</h1>
-                    <div className='account-wall'>
-                        <form className='form-signin'>
-                        <label>Username</label>
-                        <input type='text' class='form-control' required autofocus></input>
-                        <label>Password</label>
-                        <input type='password' class='form-control' required></input>
-                        <label>
-                        <input type='checkbox' name='remember'></input>
-                            Remember me
-                        </label>
-                        <a className='btn btn-lg btn-outline-primary btn-block' href='./management.html' type='submit'>
-                        Sign In
-                        </a>
-                        </form>
-                    </div>
+                <div className='row justify-content-center align-items-center'>
+                    <Graphic src = {image} alt = 'Whitworth Logo'/>
+                </div>
+                <div className="row justify-content-center align-items-center">
+                    {/* Sign in View */}
+                    {!this.state.signUp &&
+                        <div className="SignInView">
+                            <h1 className='text-center login-title'>Sign in to continue</h1>
+                            <div className='account-wall'>
+                                <form className='form-signin'>
+                                    <label>Username</label>
+                                    <input type='text' class='form-control' required autofocus></input>
+                                    <label>Password</label>
+                                    <input type='password' class='form-control' required></input>
+                                    <label>
+                                    <input type='checkbox' name='remember'></input>
+                                        Remember me
+                                    </label>
+                                    <button className='btn btn-lg btn-outline-primary btn-block' href='./management.html' type='submit'>
+                                        Sign In
+                                    </button>
+                                </form>
+                                <BottomButton className="btn btn-outline-secondary btn-block" onClick={this.toggleSignUpView}>Sign Up</BottomButton>
+                            </div>
+                        </div>
+                    }
+                    {/* Sign Up View */}
+                    {this.state.signUp &&
+                        <div className="SignUpView">
+                            <h1 className='text-center login-title'>Sign Up to continue</h1>
+                            <div className='account-wall'>
+                                <form className='form-signup'>
+                                    <label>Username</label>
+                                    <input type='text' class='form-control' required autofocus></input>
+                                    <label>Password</label>
+                                    <input type='password' class='form-control' required></input>
+                                    <label>Confirm Password</label>
+                                    <input type='password' class='form-control' required></input>
+                                    <label>
+                                    <input type='checkbox' name='remember'></input>
+                                        Remember me
+                                    </label>
+                                    <button className='btn btn-lg btn-outline-primary btn-block' href='./management.html' type='submit'>
+                                        Sign Up
+                                    </button>
+                                </form>
+                                <BottomButton className="btn btn-outline-secondary btn-block" onClick={this.toggleSignUpView}>Go Back to Sign In</BottomButton>
+                            </div>
+                        </div>
+                    }
+                </div>
             </div>
         );
     }
