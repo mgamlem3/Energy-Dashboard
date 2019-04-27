276b661f0cbec66ac6ae89076de0df4a4f341d28
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-27 19:21:18 +0000

    Deploy mgamlem3/Energy-Dashboard to github.com/mgamlem3/Energy-Dashboard.git:gh-pages

--- display.js
+++ display.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd6a44944f6764ffd4ec";
+/******/ 	var hotCurrentHash = "d259704787647e8057db";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
