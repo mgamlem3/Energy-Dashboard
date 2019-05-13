2e04a18fee9e413c299224396b20223c7a3ac72a
Michael Gamlem III <mgamlem3@gmail.com>
2019-05-13 16:50:44 -0700

    Fix power cost bug

--- src/components/ManagementDetails/management-details.jsx
+++ src/components/ManagementDetails/management-details.jsx
@@ -5,17 +5,25 @@ import { getPowerCost, updatePowerCost } from "../../database functions/manageme
 class ManagementDetails extends React.Component {
     state = {
         energyCost: "",
+        newEnergyCost: "",
     }
 
     async componentDidMount() {
-        updatePowerCost(0.086); // todo: remove this line when the actual value is stored in the release database
-        var cost = await getPowerCost();
-        this.setState({energyCost: cost});
-        console.log(this.state.energyCost);
+        var cost = await this.getPowerCostAsync();
+        this.setState({energyCost: cost.data[0].cost});
     }
 
-    changeEnergyCost() {
-        updatePowerCost(0.086); // todo: make this a user-enterable text box
+    async getPowerCostAsync() {
+        var cost = await getPowerCost();
+        return cost;
+    }
+
+    changeEnergyCost = () => {
+        updatePowerCost(this.state.newEnergyCost);
+    }
+
+    updateEnergyCostValue = (event) => {
+        this.setState({newEnergyCost: event.target.value});
     }
 
     render() {
@@ -24,10 +32,10 @@ class ManagementDetails extends React.Component {
                 <div className='d-flex flex-row no-gutters'>
                     <div className='col-sm-6'>
                         <div className='d-flex flex-row no-gutters'>
-                            <h1>Current Energy Cost: $0.086/kWh</h1>
-                            {/* <h1>Current Energy Cost: ${this.state.energyCost.data}/kWh</h1> */}
+                            <h1>{`Current Energy Cost $${this.state.energyCost}/KWH`}</h1>
                         </div>
                         <div className='d-flex flex-row no-gutters'>
+                            <input type='text' className='form-control' value={this.state.newEnergyCost} onChange={this.updateEnergyCostValue}></input>
                             <button className='btn btn-lg btn-outline-primary' onClick={this.changeEnergyCost}>Change Energy Cost</button>
                         </div>
                     </div>
