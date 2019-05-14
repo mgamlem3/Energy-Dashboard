1bc7dc0b3affd98256864969698e2ac237cba0d7
Andrew Hutson <andrew.hutson@outlook.com>
2019-05-13 18:14:37 -0700

    Format Energy Cost Management Adjust text box size and add description and instructions Add conditional to display success message or instructions for changing energy cost Add note in User Management section about Firebase bug

--- src/components/ManagementDetails/management-details.jsx
+++ src/components/ManagementDetails/management-details.jsx
@@ -6,6 +6,9 @@ class ManagementDetails extends React.Component {
     state = {
         energyCost: "",
         newEnergyCost: "",
+        success1: false,
+        success2: false,
+        successMessage: "Energy Cost Successfully Changed. Please Reload the Page"
     }
 
     async componentDidMount() {
@@ -20,10 +23,25 @@ class ManagementDetails extends React.Component {
 
     changeEnergyCost = () => {
         updatePowerCost(this.state.newEnergyCost);
+        this.showSuccessBanner1();
     }
 
     updateEnergyCostValue = (event) => {
         this.setState({newEnergyCost: event.target.value});
+        this.hideSuccessBanner1();
+        this.showSuccessBanner2();
+    }
+
+    showSuccessBanner1 = () => {
+        this.setState({success1: true});
+    }
+
+    hideSuccessBanner1 = () => {
+        this.setState({success1: false});
+    }
+
+    showSuccessBanner2 = () => {
+        this.setState({success2: true});
     }
 
     render() {
@@ -32,16 +50,53 @@ class ManagementDetails extends React.Component {
                 <div className='d-flex flex-row no-gutters'>
                     <div className='col-sm-6'>
                         <div className='d-flex flex-row no-gutters'>
-                            <h1>{`Current Energy Cost $${this.state.energyCost}/KWH`}</h1>
+                            <h1>{`Current Energy Cost $${this.state.energyCost}/kWh`}</h1>
                         </div>
+                        <div className='row mt-2'></div>
                         <div className='d-flex flex-row no-gutters'>
-                            <input type='text' className='form-control' value={this.state.newEnergyCost} onChange={this.updateEnergyCostValue}></input>
-                            <button className='btn btn-lg btn-outline-primary' onClick={this.changeEnergyCost}>Change Energy Cost</button>
+                            <div className='col-sm-3'>
+                                <h4>New Energy Cost: $</h4>
+                            </div>
+                            <div className='col-sm-1'>
+                                <input type='text' className='form-control' value={this.state.newEnergyCost} onChange={this.updateEnergyCostValue} placeholder={this.state.energyCost}></input>
+                            </div>
+                            <div className='col-sm-1'>
+                                <h4>/kWh</h4>
+                            </div>
+                            <div className='col-sm-7'>
+                                <button className='btn btn-md btn-outline-primary' onClick={this.changeEnergyCost}>Change Energy Cost</button>
+                            </div>
                         </div>
+                        <div className='row mt-2'></div>
+                        {this.state.success1 && this.state.success2 &&
+                        <div className="alert alert-success" role="alert">
+                            {this.state.successMessage}
+                        </div>
+                        }
+                        {(!this.state.success1 || !this.state.success2) &&
+                        <div>
+                            <div className='d-flex flex-row no-gutters'>
+                            <h2>To change the energy cost used by the dashboard:</h2>
+                            </div>
+                            <div className='d-flex flex-row no-gutters'>
+                                1. Enter the new energy cost in the text box above
+                            </div>
+                            <div className='d-flex flex-row no-gutters'>
+                                2. Click "Change Energy Cost"
+                            </div>
+                            <div className='d-flex flex-row no-gutters'>
+                                3. Reload the page to see the result.
+                            </div>
+                        </div>
+                        }
+                        
                     </div>
                     <div className='col-sm-6'>
                         <div className='d-flex flex-row no-gutters'>
                             <h1>User Management</h1>
+                        </div>
+                        <div className='d-flex flex-row no-gutters'>
+                            Local User Management is currently disabled due to a Google Firebase Bug
                         </div>
                     </div>
                 </div>
